2882f72e9e4e15e64d77e7493f12c38a2c6ec524
Nachiket Lele <nachisnfx@gmail.com>
2021-11-24 10:17:57 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -128,7 +128,9 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Department = devops
+    Department = devops    
+    Billable = true
+
   }
 }
 
